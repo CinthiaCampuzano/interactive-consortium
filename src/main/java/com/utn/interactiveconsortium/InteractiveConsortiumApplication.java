fc6c57c23d7770400741c4b50a14766938f66e7a
cinth <cinthialujancampuzano@gmail.com>
2024-04-15 21:06:59 -0300

    Start

---
+++ src/main/java/com/utn/interactiveconsortium/InteractiveConsortiumApplication.java
@@ -0,0 +1,13 @@
+package com.utn.interactiveconsortium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveConsortiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveConsortiumApplication.class, args);
+	}
+
+}
